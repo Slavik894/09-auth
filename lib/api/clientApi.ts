bc764dc8705f2e24969c0fbeafe958d53bafb1c5
Slavik <slavik1058@gmail.com>
2026-08-28 23:58:23 +0200

    feat: Add sign-up and sign-in pages with corresponding styles and API integration

--- lib/api/clientApi.ts
+++ lib/api/clientApi.ts
@@ -40,3 +40,23 @@ export const deleteNote = async(noteId: string)=>{
     const res = await nextServer.delete<Note>(`/notes/${noteId}`);
     return res.data
 }
+
+export type RegisterRequest = {
+  email: string;
+  password: string;
+  userName: string;
+};
+
+export type User = {
+  id: string;
+  email: string;
+  userName?: string;
+  photoUrl?: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export const register = async (data: RegisterRequest) => {
+  const res = await nextServer.post<User>('/auth/register', data);
+  return res.data;
+};
