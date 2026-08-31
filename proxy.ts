d2c8aa400ccd39bb79e82b1dd56ae446f2f57086
Slavik <slavik1058@gmail.com>
2026-08-31 02:01:48 +0200

    fix: Refactor cookie handling in proxy to improve clarity and functionality

--- proxy.ts
+++ proxy.ts
@@ -28,7 +28,8 @@ export async function proxy(request: NextRequest) {
             const parsed = parseSetCookie(cookieStr);
             
             if (parsed.value) {
-		      cookieStore.set(parsed.name, parsed.value, parsed);
+		      const { name, value, ...options } = parsed;
+          cookieStore.set(name, value, options);
 			}
     }
           if (isPublicRoute) {
@@ -50,6 +51,10 @@ export async function proxy(request: NextRequest) {
            catch{
 if(isPrivateRoute){
   return NextResponse.redirect(new URL('/sign-in', request.url));
+}
+      if (isPublicRoute) {
+      return NextResponse.next();
+    }
 }
 }
       }
